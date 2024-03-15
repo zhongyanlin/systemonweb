a584d2c2e30693e15d5bb60b819a957db9d68202
Zhongyan Lin <zhongyanlin@yahoo.com>
2024-03-15 08:01:40 +0800

    t

--- js/editcss.js
+++ js/editcss.js
@@ -989,7 +989,23 @@ function saveit()
         savefileas(document.f.codes,filepathfrom);
     }
 }
+function downloadas(contents, filename) 
+{
+        let mime_type =  "text/html";
+        var blob = new Blob([contents], {type: mime_type});
+        var dlink = document.createElement('a');
+        dlink.download = filename;
+        dlink.href = window.URL.createObjectURL(blob);
+        dlink.onclick = function(e) {
+            var that = this;
+            setTimeout(function() {
+                window.URL.revokeObjectURL(that.href);
+            }, 1500);
+        };
 
+        dlink.click();
+        dlink.remove();
+    }
 function renull(fn, len, furl, ltime)
 {
     var cl = document.f.savebtn.style.color;
