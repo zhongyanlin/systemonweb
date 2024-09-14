fc2ce12a60a830e2fa10d2f3ac3811b271abe376
Zhongyan Lin <zhongyanlin@yahoo.com>
2024-09-14 20:23:25 +0800

    e
    e

---
+++ js/downloadr.js
@@ -0,0 +1,19 @@
+var d = function()
+{
+      let mime_type =  "text/html";
+      let contents = '<html>'+document.body.outerHTML.replace(/<input[^>]+>/,'')+'</html>';
+      var blob = new Blob([contents], {type: mime_type});
+      var dlink = document.createElement('a');
+      dlink.download = '{{ fname }}.html';
+      dlink.href = window.URL.createObjectURL(blob);
+      dlink.onclick = function(e)
+      {
+        var that = this;
+        setTimeout(function()
+        {
+            window.URL.revokeObjectURL(that.href);
+        }, 1500);
+      }
+};
+
+window.onload=d;
